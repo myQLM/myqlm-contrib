9e4831e0a5332924524d288d9d1e77e86e6efe73
Arnaud Gazda <arnaud.gazda@atos.net>
2020-09-08 08:14:53 +0000

    Improve setup.py

--- setup.py
+++ setup.py
@@ -19,11 +19,33 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
+def get_description():
+    """
+    Returns the long description of the project.
+    The description of the project is written in
+    the README.md file
+
+    Returns:
+        str: long description of the project
+    """
+    with open("README.md", "r") as readme:
+        return readme.read()
+
+
 setup(
     name="myqlm-contrib",
-    version="0.0.6",
+    version="1.0.0",
     author="Atos Quantum Lab",
+    author_email="myqlm@atos.net",
+    description="Modules developed by the myQLM community",
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
     license="Atos myQLM EULA",
+    url="https://atos.net/en/lp/myqlm",
+    project_urls={
+        "Documentation": "https://myqlm.github.io",
+        "Source Code": "https://github.com/myQLM/myqlm-contrib"
+    },
     packages=find_namespace_packages(include=["qat.*"]),
     test_suite="tests",
     install_requires=["qat-core",
