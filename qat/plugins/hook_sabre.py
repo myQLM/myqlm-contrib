a1b4668ea0632fa8df2388e5b119b67a9d65a4a0
tayral <thomas.ayral@atos.net>
2021-03-01 16:09:39 +0100

    Add license.

--- qat/plugins/hook_sabre.py
+++ qat/plugins/hook_sabre.py
@@ -4,12 +4,26 @@
 @authors   Quentin Delamea <quentin.delamea@atos.net>
 @intern
 @copyright 2020 Bull S.A.S.  -  All rights reserved.
-           This is not Free or Open Source software.
-           Please contact Bull SAS for details about its license.
-           Bull - Rue Jean Jaurès - B.P. 68 - 78340 Les Clayes-sous-Bois
 @file qat/plugins/hook_sabre.py
 @brief A simple hook to import plugins of qat.sabre
 @namespace qat.sabre
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
 """
 
 from qat.sabre import Sabre
